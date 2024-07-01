a55d3deea81442b5e637ef075492ab8c3086e81d
im-vishal <iamvirtualvishal@gmail.com>
2024-07-01 12:52:58 +0530

    data_ingestion stage added

---
+++ src/__init__.py
@@ -0,0 +1,19 @@
+import logging
+
+
+# Logging Configuration
+logger = logging.getLogger('data ingestion')
+logger.setLevel('DEBUG')
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel('DEBUG')
+
+file_handler = logging.FileHandler('errors.log')
+file_handler.setLevel('ERROR')
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
